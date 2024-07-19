ea7176a4e9f89da6c13393ea2d86d7d6a03b3d48
Adelodun Damilare <adelodundamilare@gmail.com>
2024-07-19 22:37:40 +0100

    add landing page ui

---
+++ components/logos.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import Image from "next/image";
+
+const logos = [
+  {
+    src: "/images/CC-logo-white.png",
+    alt: "Cardano Casino",
+    srcSet: "/images/CC-logo-white@2x.png 2x, /images/CC-logo-white@3x.png 3x",
+  },
+  {
+    src: "/images/CARDANO-THOR-LOGOWORDS-ONLY-WHITE.png",
+    alt: "Cardano Tavern",
+    srcSet:
+      "/images/CARDANO-THOR-LOGOWORDS-ONLY-WHITE@2x.png 2x, /images/CARDANO-THOR-LOGOWORDS-ONLY-WHITE@3x.png 3x",
+  },
+  {
+    src: "/images/Goombles-logo-light.png",
+    alt: "Goombles",
+    srcSet:
+      "/images/Goombles-logo-light@2x.png 2x, /images/Goombles-logo-light@3x.png 3x",
+  },
+  {
+    src: "/images/Snekkies-logo-light.png",
+    alt: "Snekkes",
+    srcSet:
+      "/images/Snekkies-logo-light@2x.png 2x, /images/Snekkies-logo-light@3x.png 3x",
+  },
+  {
+    src: "/images/frigid-logo-white.png",
+    alt: "Frigid",
+    srcSet:
+      "/images/frigid-logo-white@2x.png 2x, /images/frigid-logo-white@3x.png 3x",
+  },
+  {
+    src: "/images/MVPz_white_transparent_bg.png",
+    alt: "MVPz",
+    srcSet:
+      "/images/MVPz_white_transparent_bg@2x.png 2x, /images/MVPz_white_transparent_bg@3x.png 3x",
+  },
+];
+
+export default function Logos() {
+  return (
+    <div className="absolute bottom-4 flex justify-center space-x-4 mt-8 w-full">
+      <div className="logos group relative overflow-hidden whitespace-nowrap py-10 [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
+        <div className="animate-slide-left-infinite group-hover:animation-pause inline-block w-max">
+          {logos.map((logo, index) => (
+            <Image
+              key={index}
+              src={logo.src}
+              alt={logo.alt}
+              sizes={logo.srcSet}
+              className="mx-4 inline opacity-50 hover:opacity-100"
+              width={100}
+              height={50}
+            />
+          ))}
+        </div>
+
+        <div className="animate-slide-left-infinite group-hover:animation-pause inline-block w-max">
+          {logos.map((logo, index) => (
+            <Image
+              key={index}
+              src={logo.src}
+              alt={logo.alt}
+              sizes={logo.srcSet}
+              className="mx-4 inline opacity-50 hover:opacity-100"
+              width={100}
+              height={50}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,10 @@
-import type { Config } from "tailwindcss"
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
-  prefix: "",
+  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
@@ -52,29 +40,33 @@ const config = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
+        orange: {
+          500: "#f97316",
+        },
       },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+      fontFamily: {
+        sans: ["var(--font-source-sans-pro)", ...fontFamily.sans],
       },
       keyframes: {
+        "slide-left": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          to: { height: 0 },
         },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-left": "slide-left 8s linear infinite",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+};
